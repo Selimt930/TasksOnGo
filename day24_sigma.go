00d692d57fb1b8a11d926ae0ecf04dcefe37dc0d
Selim Tokhtarov <selimtokhtarov@Selims-MacBook-Pro.local>
2020-09-09 14:27:41 +0300

    24 commit

--- day24_sigma.go
+++ day24_sigma.go
@@ -1,17 +1,52 @@
 //https://www.hackerrank.com/challenges/mini-max-sum/problem
 package main
 
-    import (
-        "fmt"
-        "sort"
-    )
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "sort"
+)
 
-    func main(){
+// Complete the miniMaxSum function below.
+func miniMaxSum(arr []int) {
+sort.Ints(arr) 
+min := arr[0]+arr[1]+arr[2]+arr[3]
+max := arr[1]+arr[2]+arr[3]+arr[4]
+fmt.Println(min,max)
+}
 
-        array := make([]int, 5)
-        fmt.Scan(&array[0],&array[1],&array[2],&array[3],&array[4]) 
-        sort.Ints(array) 
-        min := array[0]+array[1]+array[2]+array[3]
-        max := array[1]+array[2]+array[3]+array[4]
-        fmt.Println(min,max)
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    arrTemp := strings.Split(readLine(reader), " ")
+
+    var arr []int
+
+    for i := 0; i < 5; i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int(arrItemTemp)
+        arr = append(arr, arrItem)
     }
+
+    miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
