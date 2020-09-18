0ba69a736f93a26545593afd75dc5715ca9a0bdb
Selim Tokhtarov <selimtokhtarov@Selims-MacBook-Pro.local>
2020-09-18 11:39:47 +0300

    33 commit

---
+++ day33_sigma.go
@@ -0,0 +1,75 @@
+//https://www.hackerrank.com/challenges/repeated-string/problem?h_r=internal-search
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the repeatedString function below.
+func repeatedString(s string, n int64) int64 {
+
+var l int = len(s)
+
+    times := int(n) / l
+    itr := int(n) % l
+
+    res := 0
+
+    for i := 0; i < l; i++ {
+        if s[i] == 'a' {
+            res++
+        }
+    }
+
+    res = res * times
+
+    for i := 0; i < itr; i++ {
+        if s[i] == 'a' {
+            res++
+        }
+    }
+    return int64(res)
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    s := readLine(reader)
+
+    n, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+
+    result := repeatedString(s, n)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
