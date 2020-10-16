c99659fbf4809d58213fe16c0e3ce7f6fec70657
Selim Tokhtarov <selimtokhtarov@Selims-MacBook-Pro.local>
2020-10-16 12:12:42 +0300

    61

---
+++ day61_sigma.go
@@ -0,0 +1,72 @@
+//https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?h_r=internal-search
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the designerPdfViewer function below.
+func designerPdfViewer(h []int32, word string) int32 {
+var ln = len(word)
+var heigh int32 
+var res int32
+    var mm int32 = h[word[0] - 'a']
+    for i := 1; i < len(word); i++ {
+        heigh = h[word[i] - 'a']
+        if heigh > mm {
+            mm = heigh
+        }
+    }
+    res = mm * int32(ln)
+    return res
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    hTemp := strings.Split(readLine(reader), " ")
+
+    var h []int32
+
+    for i := 0; i < 26; i++ {
+        hItemTemp, err := strconv.ParseInt(hTemp[i], 10, 64)
+        checkError(err)
+        hItem := int32(hItemTemp)
+        h = append(h, hItem)
+    }
+
+    word := readLine(reader)
+
+    result := designerPdfViewer(h, word)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
