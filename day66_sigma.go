c37726a7702f3c2100c23ec3c7c3f73673a96b3b
Selim Tokhtarov <selimtokhtarov@Selims-MacBook-Pro.local>
2020-10-21 13:21:53 +0300

    66

---
+++ day66_sigma.go
@@ -0,0 +1,70 @@
+//https://www.hackerrank.com/challenges/find-digits/problem?h_r=internal-search
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the findDigits function below.
+func findDigits(n int32) int32 {
+var res int32  
+var val int32 = n
+var temp int32
+    for val != 0 {
+        temp = val % 10
+        if temp != 0 {
+            if n % temp == 0 {
+                res++
+            }
+        }
+        val /= 10
+    }
+    return res
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    t := int32(tTemp)
+
+    for tItr := 0; tItr < int(t); tItr++ {
+        nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+        checkError(err)
+        n := int32(nTemp)
+
+        result := findDigits(n)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
