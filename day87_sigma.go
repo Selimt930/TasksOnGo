7b888dad2c5387d0a37cf435752a9c97698ed218
Selim Tokhtarov <selimtokhtarov@Selims-MacBook-Pro.local>
2020-11-11 10:38:34 +0200

    87

---
+++ day87_sigma.go
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/sparse-arrays/problem?h_r=internal-search
+package main
+
+import "fmt"
+
+func main() {
+    var N, Q int
+    var line string
+
+    fmt.Scan(&N)
+    words := make(map[string]int)
+
+    for ; N > 0; N-- {
+        fmt.Scan(&line)
+        words[line]++
+    }
+
+    fmt.Scan(&Q)
+    for ; Q > 0; Q-- {
+        fmt.Scan(&line)
+        fmt.Println(words[line])
+    }
+}
